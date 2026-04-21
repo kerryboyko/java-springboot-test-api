ac884210be3c49c542962d3b01d234bc64a036cf
kerryboyko <kerry.boyko@gmail.com>
2026-04-21 15:24:15 +0100

    First Commit

---
+++ src/main/java/com/example/my_app/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.my_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Kerry Ann. Server is alive.";
+    }
+}
